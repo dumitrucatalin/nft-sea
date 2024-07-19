9bd96a2f9e8e321240590220a578baaecec9973e
Catalin Dumitru <dumitru.catalin96@yahoo.com>
2024-07-19 14:49:46 +0300

    Delete marketplace.tsx Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,8 @@
 // pages/_app.tsx
 import '../styles/globals.css';
 import type { AppProps } from "next/app";
-import { WagmiProvider } from 'wagmi';
-import { config } from '../config';
-import { GlobalStateProvider } from '../context/GlobalStateProvider';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Providers from '../app/providers';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchInterval: false,
-      refetchOnWindowFocus: false,
-      refetchOnMount: false,
-      refetchOnReconnect: false,
-      retry: false,
-      refetchIntervalInBackground: false,
-      retryOnMount: false,
-    },
-  },
-});
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.log('inside');
-
   return (
     <Component {...pageProps} />
   );
